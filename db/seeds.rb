2a84bb8febfefb9ab26a7f746a8df127e693ef9f
Rob Kaminsky <rob@mojotech.com>
2017-01-18 14:21:34 -0500

    Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_list = [
+  [ "Sam", "Smith", "SamS@example.com"],
+  [ "Jim", "Jones", "JJones@example.com"],
+  [ "Chris", "Canes", "Canes@example.com"],
+  [ "Walter", "White", "Cooker@example.com"],
+]
+
+question_list = [
+  ["What color is the color blue?"],
+  ["What is 2 + 3?"],
+  ["What smells like oranges?"]
+]
+
+answer_option_list = [
+  [1, "Blue", true],
+  [1, "Red", false],
+  [1, "Green", false],
+  [1, "Black", false],
+  [1, "White", false],
+  [2, "5", true],
+  [2, "23", false],
+  [2, "47", false],
+  [2, "643", false],
+  [3, "Oranges", true],
+  [3, "Bananas", false],
+  [3, "Pickles", false],
+  [3, "Used Sneakers", false],
+]
+
+user_answer_list = [
+  [1, 1],
+  [1, 6],
+  [1, 11],
+  [2, 2],
+  [2, 6],
+  [2, 12],
+  [3, 1],
+  [3, 6],
+  [3, 11],
+  [4, 1],
+  [4, 8],
+  [4, 10]
+]
+
+question_list.each do |content|
+  Question.find_or_create_by( content: content )
+end
+
+user_list.each do |fname, lname, email|
+  User.find_or_create_by( fname: fname, lname: lname, email: email )
+end
+
+answer_option_list.each do |question_id, content, is_correct|
+  AnswerOption.find_or_create_by( question_id: question_id, content: content, is_correct: is_correct )
+end
+
+user_answer_list.each do |user_id, answer_option_id|
+  UserAnswer.find_or_create_by( user_id: user_id, answer_option_id: answer_option_id )
+end
