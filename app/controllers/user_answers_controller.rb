775548ce52d92ea75bb5cdd85f77cafccbe3e3b6
Rob Kaminsky <rob@mojotech.com>
2017-01-24 14:31:32 -0500

    UserAnswer controller and logic

---
+++ app/controllers/user_answers_controller.rb
@@ -0,0 +1,26 @@
+class UserAnswersController < ApplicationController
+
+  def index
+  end
+
+  def create
+    question.answer_for_user(current_user, answer_option)
+    redirect_to next_question
+  end
+
+  def new
+  end
+
+  private def question
+    @question ||= Question.find(params[:question_id])
+  end
+
+  private def answer_option
+    @answer_option ||= question.answer_options.where(id: params[:user_answer][:answer_option_id]).first
+  end
+
+  private def next_question
+    #the or will be the behavior for after the last.
+    question.next || Question.first
+  end
+end
