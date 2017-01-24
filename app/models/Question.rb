ec29b3fd523287c5f772b2702d8fb66915a0f8ba
Rob Kaminsky <rob@mojotech.com>
2017-01-24 14:31:50 -0500

    changes to question model for functionality

--- app/models/Question.rb
+++ app/models/Question.rb
@@ -1,5 +1,14 @@
 class Question < ActiveRecord::Base
   has_many :answer_options
+  has_many :user_answers, through: :answer_options
 
   validates :content, presence: true
+
+  def answer_for_user (user, answer_option)
+    UserAnswer.create(user_id: user.id, answer_option_id: answer_option.id)
+  end
+
+  def next
+    Question.where("id > ?", self.id).order("id ASC").first
+  end
 end
