086fa30ca1178c257604bbc342cdafe264664136
Rob Kaminsky <rob@mojotech.com>
2017-01-24 14:03:15 -0500

    Added questions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
 
   root 'welcome#index'
+
+  resources :questions do
+    resources :user_answers, only: [:create]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
