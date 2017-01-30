c8491a269415a176b1bd24b1f1b591341a0f44e1
Rob Kaminsky <rob@mojotech.com>
2017-01-30 11:43:30 -0500

    Clean up files naming convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
+  get 'shared/index'
 
-  root 'welcome#index'
+  root 'shared#index'
 
   resources :questions do
     resources :user_answers, only: [:create]
--- test/controllers/shared_controller_test.rb
+++ test/controllers/shared_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class WelcomeControllerTest < ActionDispatch::IntegrationTest
+class SharedControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get welcome_index_url
+    get shared_index_url
     assert_response :success
   end
 
