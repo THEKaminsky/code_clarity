9dfc002d4358e4bbb66fd383b3c52ded9825271e
Rob Kaminsky <rob@mojotech.com>
2017-02-06 15:48:30 -0500

    Add duration_in_seconds to the model

--- db/migrate/20170131191255_add_time_columns_to_attempts.rb
+++ db/migrate/20170131191255_add_time_columns_to_attempts.rb
@@ -1,6 +1,6 @@
 class AddTimeColumnsToAttempts < ActiveRecord::Migration[5.0]
   def change
     add_column :attempts, :start_time, :datetime, null: false
-    add_column :attempts, :end_time, :datetime
+    add_column :attempts, :duration_in_seconds, :float
   end
 end
